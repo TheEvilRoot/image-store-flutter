0eb7a634c8483136fc7be9743a54532358e57dc5
TheEvilRoot <robot-creatix@ya.ru>
2021-07-26 00:18:35 +0300

    image loading and image cache

--- lib/image.dart
+++ lib/image.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'model.dart' as m;
@@ -13,8 +15,6 @@ class ImageComponent extends StatefulWidget {
   State<StatefulWidget> createState() {
     return ImageComponentState(model, image);
   }
-
-
 }
 
 class ImageComponentState extends State {
@@ -27,23 +27,30 @@ class ImageComponentState extends State {
   @override
   void initState() {
     super.initState();
-    _model.fetchContent(image)
-    .then((value) => setState(() {
-      image.content = value;
-    }));
+    _model.getImageFile(image)
+    .then((value) {
+      if (this.mounted)
+        setState(() {
+          image.localFile = value;
+        });
+      else print("not mounted");
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final view = image.content == null ?
-      Text("Loading...") :
-      Image.memory(image.content!);
+    final ratio = image.width.toDouble() / image.height.toDouble();
+    final maxWidth = min(MediaQuery.of(context).size.width, image.width.toDouble());
+    final maxHeight = maxWidth / ratio;
+    final view = image.localFile == null ? CircularProgressIndicator() :
+      Image.file(image.localFile!);
+
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          view,
+          SizedBox(width: maxWidth, height: maxHeight, child: Center(child: view)),
           Text(image.imageTitle),
         ],
       ),
--- lib/model.dart
+++ lib/model.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 class Image {
   final int imageId;
@@ -9,17 +11,21 @@ class Image {
   final String imageType;
   final String imageHash;
   final String imageTitle;
+  final int width;
+  final int height;
 
-  Uint8List? content;
+  File? localFile;
 
-  Image(this.imageId, this.authorId, this.imageType, this.imageHash, this.imageTitle);
+  Image(this.imageId, this.authorId, this.imageType, this.imageHash, this.imageTitle, this.width, this.height);
 
   Image.fromJson(Map<String, dynamic> json)
     : imageId = json['imageId'],
       authorId = json['imageAuthorId'],
       imageType = json['imageType'],
       imageHash = json['imageHash'],
-      imageTitle = json['imageTitle'];
+      imageTitle = json['imageTitle'],
+      width = json['width'],
+      height = json['height'];
 
   Map<String, dynamic> toJson() {
     return {
@@ -27,7 +33,9 @@ class Image {
       "imageAuthorId": authorId,
       "imageType": imageType,
       "imageHash": imageHash,
-      "imageTitle": imageTitle
+      "imageTitle": imageTitle,
+      'width': width,
+      'height': height
     };
   }
 
@@ -35,14 +43,32 @@ class Image {
 
 class Model {
 
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
   Future<List<Image>> fetchImages() async {
     final resp = await http.get(Uri.parse("http://192.168.100.6:8082/image/all"));
     final List<dynamic> json = jsonDecode(resp.body);
     return json.map((e) => Image.fromJson(e)).toList();
   }
 
-  Future<Uint8List> fetchContent(Image image) async {
+  Future<File?> getImageFile(Image image) async {
+    final dirPath = await _localPath;
+    final file = File("$dirPath/${image.imageHash}.bin");
+    if (await file.exists())
+      return file;
+    return fetchContent(file, image);
+  }
+
+  Future<File> fetchContent(File file, Image image) async {
     final resp = await http.get(Uri.parse("http://192.168.100.6:8082/image/content/${image.imageId}"));
-    return resp.bodyBytes;
+    return store(file, resp.bodyBytes);
+  }
+
+  Future<File> store(File file, Uint8List data) async {
+    return file.writeAsBytes(data);
   }
 }
